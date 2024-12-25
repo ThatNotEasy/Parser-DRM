88e6e219f5db51921c68adfbf0c965512c415efc
YOLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <apid9755@gmail.com>
2024-12-26 02:46:11 +0800

    Create banners.py

---
+++ modules/banners.py
@@ -0,0 +1,28 @@
+import os
+from sys import stdout
+from colorama import Fore
+
+def banners():
+    stdout.write("                                                                                         \n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██████╗ ██████╗ ███╗   ███╗      ██████╗  █████╗ ██████╗ ███████╗███████╗██████╗\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██╔══██╗██╔══██╗████╗ ████║      ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔══██╗\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██║  ██║██████╔╝██╔████╔██║█████╗██████╔╝███████║██████╔╝███████╗█████╗  ██████╔╝\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██║  ██║██╔══██╗██║╚██╔╝██║╚════╝██╔═══╝ ██╔══██║██╔══██╗╚════██║██╔══╝  ██╔══██╗\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██████╔╝██║  ██║██║ ╚═╝ ██║      ██║     ██║  ██║██║  ██║███████║███████╗██║  ██║\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝      ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝\n")
+    stdout.write(""+Fore.YELLOW +"═════════════╦═════════════════════════════════╦══════════════════════════════\n")
+    stdout.write(""+Fore.YELLOW   +"╔════════════╩═════════════════════════════════╩═════════════════════════════╗\n")
+    stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"AUTHOR             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   PARI MALAM                                    "+Fore.YELLOW+"║\n")
+    stdout.write(""+Fore.YELLOW   +"╔════════════════════════════════════════════════════════════════════════════╝\n")
+    stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"GITHUB             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   GITHUB.COM/THATNOTEASY                        "+Fore.YELLOW+"║\n")
+    stdout.write(""+Fore.YELLOW   +"╚════════════════════════════════════════════════════════════════════════════╝\n") 
+    print(f"{Fore.YELLOW}[DRM-Parser] - {Fore.GREEN}A tool for parse Playready & Widevine CDM device - {Fore.RED}[V1.0] \n{Fore.RESET}")
+
+
+ 
+
+
+
+
+
+                                                                                 
